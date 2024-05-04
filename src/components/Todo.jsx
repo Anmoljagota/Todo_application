c96c902a2e573e1dba65ceb83e23042b50163806
Anmoljagota <anmoljagota08@gmail.com>
2024-05-04 16:47:31 +0530

    filtering added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoElements from "./TodoElements";
 import { useDispatch, useSelector } from "react-redux";
 import { Delete_Todo, Task_Success, Toggle_Todo } from "../redux/action";
 import TodoItems from "./TodoItems";
-import { Box, Flex, Heading, useToast } from "@chakra-ui/react";
+import { Box, Flex, Heading, Select, useToast } from "@chakra-ui/react";
 import nextId from "react-id-generator";
 const Todo = () => {
   const toast = useToast();
   const uniqueid = nextId();
   const dispatch = useDispatch();
   const [text, setText] = useState("");
+  const [filterData, setFilterData] = useState([]);
   const Todos = useSelector((todos) => todos.todos);
 
+  useEffect(() => {
+    setFilterData(Todos);
+  }, []);
   //function to store the input data
   const handleTodo = (e) => {
     setText(e.target.value);
   };
-
   //function to add a todo inside redux
   const handleSubmit = () => {
     if (!text) {
@@ -66,39 +69,81 @@ const Todo = () => {
     }
     return color;
   }
+
+  const Filter = (e) => {
+    if (e.target.value === "default") {
+      setFilterData(Todos);
+    } else {
+      const filterData = Todos.filter((ele) => {
+        if (e.target.value === "completed" && ele.status == true) {
+          return ele;
+        } else if (e.target.value === "notcompleted" && ele.status == false) {
+          return ele;
+        } else {
+          return "";
+        }
+      });
+      setFilterData(filterData);
+    }
+  };
   return (
-    <Box
-      bg={"#1a202c"}
-      p={6}
-      w={{ lg: "40vw", md: "60vw", sm: "90vw", base: "95vw" }}
-      borderRadius={10}
-    >
-      <Heading
-        as="h2"
-        pb={10}
-        fontSize={{sm:"1.6rem",base:"1rem"}}
-        color={"white"}
-        textAlign={"center"}
+    <>
+      <Box
+        bg={"#1a202c"}
+        p={6}
+        w={{ lg: "40vw", md: "60vw", sm: "90vw", base: "95vw" }}
+        borderRadius={10}
+        pos={"relative"}
       >
-        What's the Plan for Today?
-      </Heading>
-      <TodoElements
-        handleTodo={handleTodo}
-        handleSubmit={handleSubmit}
-        text={text}
-      />
-      <Flex flexDir={"column"} gap={"0.5rem"} mt={5}>
-        {Todos.length > 0 &&
-          Todos.map((ele) => (
-            <TodoItems
-              key={ele.id}
-              {...ele}
-              DeleteTodo={DeleteTodo}
-              ToggleTodo={ToggleTodo}
-            />
-          ))}
-      </Flex>
-    </Box>
+        <Select
+          w={{ sm: "30%", base: "50%" }}
+          onChange={Filter}
+          bg={"black"}
+          color={"white"}
+          pos={"absolute"}
+          right={5}
+          top={20}
+        >
+          <option value={"default"} style={{ background: "black" }}>
+            default
+          </option>
+          <option value={"completed"} style={{ background: "black" }}>
+            Completed
+          </option>
+          <option value={"notcompleted"} style={{ background: "black" }}>
+            Not Completed
+          </option>
+        </Select>
+        <Heading
+          as="h2"
+          pb={10}
+          fontSize={{ sm: "1.6rem", base: "1rem" }}
+          color={"white"}
+          textAlign={"center"}
+        >
+          What's the Plan for Today?
+        </Heading>
+
+        <Box mt={"3rem"}>
+          <TodoElements
+            handleTodo={handleTodo}
+            handleSubmit={handleSubmit}
+            text={text}
+          />
+          <Flex flexDir={"column"} gap={"0.5rem"} mt={5}>
+            {filterData.length > 0 &&
+              filterData.map((ele) => (
+                <TodoItems
+                  key={ele.id}
+                  {...ele}
+                  DeleteTodo={DeleteTodo}
+                  ToggleTodo={ToggleTodo}
+                />
+              ))}
+          </Flex>
+        </Box>
+      </Box>
+    </>
   );
 };
 
